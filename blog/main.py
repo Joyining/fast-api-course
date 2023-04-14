20a96d5e6ed1db75c39339155e2b1732a82a8895
Joyining <yining1204@gmail.com>
2023-04-14 16:39:02 +0800

    Post data to database

--- blog/main.py
+++ blog/main.py
@@ -1,12 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from . import schemas, models
-from .database import engine
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.post('/blog')
-def create_blog(blog: schemas.Blog):
-    return 'A blog was created.'
+def create_blog(blog: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(
+        title=blog.title,
+        body=blog.body
+    )
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog

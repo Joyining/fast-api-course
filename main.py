62260f68cd0a666101e24ab8fb85d3613064eb07
Joyining <yining1204@gmail.com>
2023-04-14 11:04:12 +0800

    Path parameters and query parameters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import Optional
 
 # create instance
 app = FastAPI()
@@ -12,3 +13,21 @@ def index():
 @app.get('/about')
 def about():
     return {'data': {'about': 'This is us.'}}
+
+
+@app.get('/blogs')
+def get_blogs(limit=50, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': {'blogs': f'Showing {limit} published blogs.'}}
+    else:
+        return {'data': {'blogs': f'Showing {limit} blogs.'}}
+
+
+@app.get('/blogs/unpublished')
+def get_unpublished_blogs():
+    return {'data': {'blogs': 'All unpublished blogs.'}}
+
+
+@app.get('/blogs/{blog_id}')
+def get_blog(blog_id: int):
+    return {'data': blog_id}

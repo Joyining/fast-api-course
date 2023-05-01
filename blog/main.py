e69a9a074f03e2f7d9c96d0ce9469cd8a54e4478
Joyining <yining1204@gmail.com>
2023-05-01 14:14:27 +0800

    Get user

--- blog/main.py
+++ blog/main.py
@@ -69,7 +69,7 @@ def update_blog(blog_id, request: schemas.Blog, db: Session = Depends(get_db)):
     return 'updated.'
 
 
-@app.post('/users', status_code=status.HTTP_201_CREATED)
+@app.post('/users', status_code=status.HTTP_201_CREATED, response_model=schemas.ShowUser)
 def create_user(user: schemas.User, db: Session = Depends(get_db)):
     new_user = models.User(
         name=user.name,
@@ -80,3 +80,12 @@ def create_user(user: schemas.User, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@app.get('/users/{user_id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowUser)
+def get_user(user_id, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not user:
+        raise HTTPException(status.HTTP_404_NOT_FOUND,
+                            detail=f'User with id {user_id} is not available.')
+    return user
